6787748a26906fba40fd9c3a533ece30fdbf8d64
nishamizh <nishamizh@gmail.com>
2024-04-23 15:11:35 -0700

    first panda from leet

--- pandas/createDF_list.py
+++ pandas/createDF_list.py
@@ -1,3 +1,19 @@
+import pandas as pd
+from typing import List
+
+list_2d = [[1,15],[2,11],[3,11],[4,20]]
+
+# alias 1 - method
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    df = pd.DataFrame(student_data, columns=['student_id','age'])
+    return df
+
+print(createDataframe(list_2d))
+
+# Alias 2
+df = pd.DataFrame(list_2d, columns=['student_id','age'])
+print(df)
+
 # Leet-2877 Create a Data from List
 #Write a solution to create a DataFrame from a 2D list called student_data.This 2D list contains the IDs and ages of some students.
 #The DataFrame should have two columns, student_id and age, and be in the same order as the original 2D list.
@@ -22,22 +38,4 @@
 # +------------+-----+
 # Explanation:
 # A DataFrame was created on top of student_data,with two columns named student_id and age.
-
-import pandas as pd
-from typing import List
-
-list_2d = [[1,15],[2,11],[3,11],[4,20]]
-
-# alias 1 - method
-def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    df = pd.DataFrame(student_data, columns=['student_id','age'])
-    return df
-
-print(createDataframe(list_2d))
-
-# Alias 2
-df = pd.DataFrame(list_2d, columns=['student_id','age'])
-print(df)
-
-
 
