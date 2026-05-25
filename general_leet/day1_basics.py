61d32a1da66a89041ea497da94f3e3c879a1cf1c
nishamizh <nishamizh@gmail.com>
2026-05-25 13:46:41 -0700

    Q4 - tree

--- general_leet/day1_basics.py
+++ general_leet/day1_basics.py
@@ -1,5 +1,6 @@
 # Practice basic Coding with me - Nisha
 # This is purely for first timers
+from collections import deque
 
 # Q1.  https://leetcode.com/problems/two-sum/ - full question description at the end of all problems
 
@@ -47,7 +48,7 @@ class Solution(object):
     result1 = isValid(s)
     print(result1)
 
-
+# Q3 https://leetcode.com/problems/binary-search/description/
     def search(nums, target):
         left,right = 0,len(nums)-1
 
@@ -69,6 +70,55 @@ class Solution(object):
     target = 9
     print(search(nums, target))
     
+
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+
+    #Q4 https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+    # using DFS
+    """
+    def maxDepthDFS(root):
+        #:type root: Optional[TreeNode]
+        #:rtype: int
+        
+        if not root:
+            return 0
+        
+        return 1+(max(Solution().maxDepthDFS(root.left),Solution().maxDepthDFS(root.right)))
+    
+"""
+#root = [3, 9, 20, None, None, 15, 7]
+#print(maxDepthDFS(root))
+
+
+# Using BFS
+def maxDepth(root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0
+        
+        q = deque([root])
+        depth = 0
+
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+            depth+=1
+
+        return depth
+
+
+root = [3,9,20,None,None,15,7]
+#print(maxDepth(root))
+
     
 
 
@@ -161,4 +211,27 @@ Constraints:
 -104 < nums[i], target < 104
 All the integers in nums are unique.
 nums is sorted in ascending order.
+"""
+
+"""
+Q4 - 104. Maximum Depth of Binary Tree
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node. 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100
 """
